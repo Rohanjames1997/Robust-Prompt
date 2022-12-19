1b03cba4f2c53c7d869ee63797a474363069730e
Rohan <rj2665@columbia.edu>
2022-12-19 16:53:29 -0500

    Initial commit

---
+++ cutout/create_and_save_data.py
@@ -0,0 +1,148 @@
+# run train.py --dataset cifar10 --model resnet18 --data_augmentation --cutout --length 16
+# run train.py --dataset cifar100 --model resnet18 --data_augmentation --cutout --length 8
+# run train.py --dataset svhn --model wideresnet --learning_rate 0.01 --epochs 160 --cutout --length 20
+
+import pdb
+import argparse
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.optim.lr_scheduler import MultiStepLR
+
+from torchvision.utils import make_grid
+from torchvision import datasets, transforms
+
+from util.misc import CSVLogger
+from util.cutout import Cutout
+
+import os
+import torchvision.transforms.functional as trnF 
+
+dataset_options = ['cifar10', 'cifar100', 'svhn']
+
+parser = argparse.ArgumentParser(description='CNN')
+parser.add_argument('--dataset', '-d', default='cifar10',
+                    choices=dataset_options)
+parser.add_argument('--batch_size', type=int, default=128,
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--data_augmentation', action='store_true', default=False,
+                    help='augment data by flipping and cropping')
+parser.add_argument('--cutout', action='store_true', default=False,
+                    help='apply cutout')
+parser.add_argument('--n_holes', type=int, default=1,
+                    help='number of holes to cut out from image')
+parser.add_argument('--length', type=int, default=16,
+                    help='length of the holes')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=0,
+                    help='random seed (default: 1)')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+cudnn.benchmark = True  # Should make training should go faster for large models
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+# test_id = args.dataset + '_' + args.model
+
+print(args)
+
+# Image Preprocessing
+if args.dataset == 'svhn':
+    normalize = transforms.Normalize(mean=[x / 255.0 for x in[109.9, 109.7, 113.8]],
+                                     std=[x / 255.0 for x in [50.1, 50.6, 50.8]])
+else:
+    normalize = transforms.Normalize(mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+                                     std=[x / 255.0 for x in [63.0, 62.1, 66.7]])
+
+train_transform = transforms.Compose([])
+if args.data_augmentation:
+    train_transform.transforms.append(transforms.RandomCrop(32, padding=4))
+    train_transform.transforms.append(transforms.RandomHorizontalFlip())
+train_transform.transforms.append(transforms.ToTensor())
+train_transform.transforms.append(normalize)
+if args.cutout:
+    train_transform.transforms.append(Cutout(n_holes=args.n_holes, length=args.length))
+
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    normalize])
+
+if args.dataset == 'cifar10':
+    num_classes = 10
+    train_dataset = datasets.CIFAR10(root='data/',
+                                     train=True,
+                                     transform=train_transform,
+                                     download=True)
+
+    test_dataset = datasets.CIFAR10(root='data/',
+                                    train=False,
+                                    transform=test_transform,
+                                    download=True)
+elif args.dataset == 'cifar100':
+    num_classes = 100
+    train_dataset = datasets.CIFAR100(root='data/',
+                                      train=True,
+                                      transform=train_transform,
+                                      download=True)
+
+    test_dataset = datasets.CIFAR100(root='data/',
+                                     train=False,
+                                     transform=test_transform,
+                                     download=True)
+elif args.dataset == 'svhn':
+    num_classes = 10
+    train_dataset = datasets.SVHN(root='data/',
+                                  split='train',
+                                  transform=train_transform,
+                                  download=True)
+
+    extra_dataset = datasets.SVHN(root='data/',
+                                  split='extra',
+                                  transform=train_transform,
+                                  download=True)
+
+    # Combine both training splits (https://arxiv.org/pdf/1605.07146.pdf)
+    data = np.concatenate([train_dataset.data, extra_dataset.data], axis=0)
+    labels = np.concatenate([train_dataset.labels, extra_dataset.labels], axis=0)
+    train_dataset.data = data
+    train_dataset.labels = labels
+
+    test_dataset = datasets.SVHN(root='data/',
+                                 split='test',
+                                 transform=test_transform,
+                                 download=True)
+
+# Data Loader (Input Pipeline)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=args.batch_size,
+                                           shuffle=True,
+                                           pin_memory=True,
+                                           num_workers=2)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=args.batch_size,
+                                          shuffle=False,
+                                          pin_memory=True,
+                                          num_workers=2)
+
+
+
+progress_bar = tqdm(train_loader)
+for i, (image, label) in enumerate(progress_bar):
+    save_path = './Cutout_CIFAR100/' + str(label.item()) + "/"
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    save_path += str(i) + ".jpeg"
+
+    with torch.no_grad():
+        img = trnF.to_pil_image(image.squeeze().clamp(0, 1))
+    img.save(save_path)
---
+++ models/download_models.sh
@@ -0,0 +1,8 @@
+# CLIP
+pip install git+https://github.com/openai/CLIP.git
+
+# Big Transfer (BiT)
+git clone https://github.com/google-research/big_transfer.git
+pip install -r big_transfer/bit_pytorch/requirements.txt
+wget https://storage.googleapis.com/bit_models/BiT-M-R50x1.npz
+wget https://github.com/chenyaofo/pytorch-cifar-models/releases/download/resnet/cifar100_resnet56-f2eff4c8.pt
